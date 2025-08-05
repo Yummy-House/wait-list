c8e19aa1770ec42467b3a49a6609edadbfe38de6
Adeniji Olajide <adenijiolajid01@gmail.com>
2025-08-05 14:08:24 +0100

    feat: enhance App layout and input validation, update package.json for development server

--- src/App.tsx
+++ src/App.tsx
@@ -114,13 +114,13 @@ function App() {
 
   return (
     <>
-      <header className="flex justify-between items-center-safe me-10 z-50 relative h-32">
-        <img src="/logo-1.svg" alt="Yummy House logo" className="logo" />
-        <button className="h-12 w-44" onClick={() => setOpenJoinWaitlist(true)}>
+      <header className="flex justify-between items-center md:me-10 me-2 z-50 relative md:h-32 h-20">
+        <img src="/logo-1.svg" alt="Yummy House logo" className="logo w-52 md:w-72" />
+        <button className="md:w-44 w-40 text-sm md:text-lg" onClick={() => setOpenJoinWaitlist(true)}>
           Join Waitlist
         </button>
       </header>
-      <main className="">
+      <main>
         {/* Dialog Backdrop */}
         {(openJoinWaitlist || openSurvey) && (
           <div
@@ -150,6 +150,7 @@ function App() {
             <input
               type="email"
               placeholder="Enter your email address"
+              required
               className="border rounded w-full h-12 ps-3 rounded-tl-4xl rounded-e-none border-[var(--primary-color)] focus:border-2 focus:bg-black bg-[var(--secondary-color)] focus:outline-none transition-all duration-300 ease-in-out text-white"
             />
             <input
@@ -282,6 +283,7 @@ function App() {
                       <input
                         type="radio"
                         name="survey-option"
+                        required
                         value={option}
                         checked={selectedAnswer === option}
                         onChange={(e) => setSelectedAnswer(e.target.value)}
@@ -296,6 +298,7 @@ function App() {
                   surveyquestion.otherPlaceholder && (
                     <input
                       type="text"
+                      required
                       placeholder={surveyquestion.otherPlaceholder}
                       value={otherAnswer}
                       onChange={(e) => setOtherAnswer(e.target.value)}
@@ -319,8 +322,9 @@ function App() {
             )}
           </form>
         </dialog>
+
         <section
-          className="p-10 pt-22 space-y-28"
+          className="md:p-10 p-4 md:pt-22 pt-5 md:space-y-28 space-y-18"
           style={{
             height: "calc(100vh - 8rem)",
           }}
@@ -353,28 +357,29 @@ function App() {
             </div>
 
             <h1
-              className="flex items-end text-3xl mb-8 relative z-10"
+              className="flex items-end md:text-3xl text-xl mb-8 relative z-10"
               style={{
                 fontFamily: "Island Moments, sans-serif",
                 color: "#FF6060",
               }}
             >
               {currentAdvert.tag}
-              <hr className="w-80 m-4" />
+              <hr className="md:w-80 w-40 md:m-4 m-3 ms-0" />
             </h1>
-            <p className="text-6xl w-1/2 mb-10 drop-shadow-md drop-shadow-amber-100 relative z-10">
+            <p className="md:text-6xl text-5xl md:w-1/2 w-full mb-10 drop-shadow-md drop-shadow-amber-100 relative z-10">
               {currentAdvert.text}
             </p>
-            <p className="font-thin text-gray-300 text-3xl drop-shadow-md drop-shadow-amber-100 relative z-10">
+            <p className="font-thin text-gray-300 md:text-3xl text-2xl drop-shadow-md drop-shadow-amber-100 relative z-10">
               {currentAdvert.subtext}
             </p>
           </div>
           <form
             onSubmit={JoinWaitlist}
-            className="flex flex-row items-center w-1/3 h-16 mt-10 relative z-10"
+            className="flex flex-row items-center lg:w-1/3 sm:w-2/3 w-full h-16 relative z-10"
           >
             <input
               type="email"
+              required
               placeholder="Enter your email address"
               className="border rounded w-full h-full ps-3 rounded-tl-4xl rounded-e-none border-[var(--primary-color)] focus:border-2 focus:bg-black bg-[var(--secondary-color)] focus:outline-none transition-all duration-300 ease-in-out text-white"
             />
@@ -386,15 +391,15 @@ function App() {
           </form>
         </section>
 
-        <section className="grid grid-cols-1 md:grid-cols-2 h-screen top-0">
-          <div className="bg-[#130404] flex flex-col items-center justify-center space-y-12 text-center">
+        <section className="grid grid-cols-1 md:grid-cols-2 top-0">
+          <div className="bg-[#130404] flex flex-col items-center justify-center space-y-12 text-center h-screen">
             <img
               src={bonAppetitImg}
               alt="Bon Appetit"
-              className="drop-shadow-xl hover:scale-110 transition-transform duration-300 hover:drop-shadow-amber-800"
+              className="drop-shadow-xl hover:scale-110 transition-transform duration-300 hover:drop-shadow-amber-800 md:w-96 w-1/2"
             />
             <div
-              className="text-9xl w-1/2 drop-shadow-xl hover:scale-110 transition-transform duration-300 hover:drop-shadow-amber-800"
+              className="lg:text-9xl md:text-7xl text-6xl w-1/2 drop-shadow-xl hover:scale-110 transition-transform duration-300 hover:drop-shadow-amber-800"
               style={{
                 fontFamily: "Tilt Prism, sans-serif",
                 fontWeight: "bold",
@@ -405,16 +410,16 @@ function App() {
             <img
               src={chefHat}
               alt="Chef Hat"
-              className="drop-shadow-xl hover:scale-110 transition-transform duration-300 hover:drop-shadow-amber-800"
+              className="drop-shadow-xl hover:scale-110 transition-transform duration-300 hover:drop-shadow-amber-800 md:w-1/6 w-1/4"
             />
           </div>
 
-          <div className="burger-bg">
-            <div className="bg-[#25201A] relative w-5/6 h-5/6 top-1/2 right-0 float-end transform -translate-y-1/2 flex flex-col justify-center items-center space-y-8 p-8">
+          <div className="burger-bg md:py-20">
+            <div className="bg-[#25201A] relative md:w-5/6 w-11/12 h-5/6 top-1/2 right-0 float-end transform -translate-y-1/2 flex flex-col justify-center items-center space-y-8 p-8 py-32">
               <div className="flex flex-row items-center justify-end space-x-8">
-                <img src={restaurantTable} alt="Restaurant Table" />
+                <img src={restaurantTable} alt="Restaurant Table" className="md:w-auto w-24" />
                 <span className="space-y-2">
-                  <div className="font-bold text-2xl">
+                  <div className="font-bold md:text-2xl text-xl">
                     Taste Meets Convenience
                   </div>
                   <div className="w-11/12">
@@ -425,9 +430,9 @@ function App() {
               </div>
 
               <div className="flex flex-row items-center justify-end space-x-8">
-                <img src={knifeandPlate} alt="Knife and Plate" />
+                <img src={knifeandPlate} alt="Knife and Plate" className="md:w-auto w-24" />
                 <span className="space-y-2">
-                  <div className="font-bold text-2xl">Dine Like Royalty</div>
+                  <div className="font-bold md:text-2xl text-xl">Dine Like Royalty</div>
                   <div className="w-11/12">
                     Explore top-rated restaurants and enjoy a fine dining
                     experience from the comfort of your home.
@@ -436,9 +441,9 @@ function App() {
               </div>
 
               <div className="flex flex-row items-center justify-end space-x-8">
-                <img src={deliveryDining} alt="Delivery Dining" />
+                <img src={deliveryDining} alt="Delivery Dining" className="md:w-auto w-24" />
                 <span className="space-y-2">
-                  <div className="font-bold text-2xl">
+                  <div className="font-bold md:text-2xl text-xl">
                     Fast & Reliable Delivery
                   </div>
                   <div className="w-11/12">
@@ -452,29 +457,29 @@ function App() {
         </section>
 
         <section className="bg-[#1A1A1A] flex h-screen">
-          <div className="w-2/12">
+          <div className="md:w-2/12 w-3/12">
             <img
               src={advertImg_1}
               alt="Dish"
               className="h-2/3 w-full object-cover"
             />
             <div className="text-center flex flex-col items-center justify-center h-1/3 space-y-2">
-              <div className="font-bold text-2xl">Set Your Table</div>
-              <div className="text-md font-thin w-2/3">
+              <div className="font-bold md:text-2xl">Set Your Table</div>
+              <div className="md:text-md font-thin w-2/3 text-xs">
                 From starters to sides, discover dishes that complete every meal
                 beautifully.
               </div>
             </div>
           </div>
-          <div className="relative w-4/12">
+          <div className="relative md:w-4/12 w-3/12">
             <img
               src={advertImg_2}
               alt="Pizza so delicious"
               className="h-full w-full object-cover"
             />
-            <div className="absolute bottom-10 p-5 space-y-2">
-              <div className="font-bold text-4xl">Pizza so delicious</div>
-              <div className="text-xl font-thin">
+            <div className="absolute bottom-10 md:p-5 p-1 space-y-2">
+              <div className="font-bold md:text-4xl text-lg">Pizza so delicious</div>
+              <div className="md:text-xl md:font-thin">
                 Melty cheese, crispy crusts, bold toppings a slice of happiness
                 in every bite
               </div>
@@ -483,8 +488,8 @@ function App() {
 
           <div className="w-3/12">
             <div className="text-center flex flex-col items-center justify-center h-1/3 space-y-2">
-              <div className="font-bold text-2xl">Savor the Flavors</div>
-              <div className="text-md font-thin w-2/3">
+              <div className="font-bold md:text-2xl">Savor the Flavors</div>
+              <div className="text-md font-thin w-2/3 text-xs">
                 Dive into a rich mix of seafood and spices, crafted for true
                 food lovers.
               </div>
@@ -503,8 +508,8 @@ function App() {
               className="h-2/3 w-full object-cover"
             />
             <div className="text-center flex flex-col items-center justify-center h-1/3 space-y-2">
-              <div className="font-bold text-2xl">Elevated Bites</div>
-              <div className="text-md font-thin w-2/3">
+              <div className="font-bold md:text-2xl">Elevated Bites</div>
+              <div className="text-md font-thin w-2/3 text-xs">
                 Perfectly plated brunches and elegant meals because every bite
                 should feel special.
               </div>
@@ -519,14 +524,14 @@ function App() {
             href="https://www.linkedin.com/in/olajide-adeniji/"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-sm font-thin text-orange-500"
+            className="md:text-sm text-xs font-thin text-orange-500"
           >
             in/olajide-adeniji
           </a>
         </div>
         <div>
-          <p>&copy; 2023 Yummy House. All rights reserved.</p>
-          <p className="text-sm font-thin">
+          <p className="text-xs">&copy; 2023 Yummy House. All rights reserved.</p>
+          <p className="md:text-sm text-xs font-thin md:block hidden">
             Discover, order, and track your favorite meals from local
             restaurants – all in one app.
           </p>
